41db85a0fc4389afec9cacc7982f5ae495c7fd77
maksim <kuzminowmax@gmail.com>
2022-05-30 19:18:16 +0300

    fix: app

--- app.js
+++ app.js
@@ -38,13 +38,15 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="input-text";
+    editInput.className="task input-text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
